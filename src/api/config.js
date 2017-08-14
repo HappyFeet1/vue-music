49ddd3fb61496de6433870c6d8aa5c58bcd087ad
wd <627498894@qq.com>
2017-08-14 13:44:00 +0800

    添加歌曲推荐

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wd on 2017/8/14.
+ */
+export const commonParms = {
+  g_tk: 5381,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+};
+export const options = {
+  param: 'jsonpCallback'
+};
+export const ERR_OK = 0;
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -3,7 +3,27 @@
 <template>
   <div class="recommend">
     推荐界面
+
+
+
+
   </div>
 </template>
 <script type="text/ecmascript-6">
+  import {getRecommend} from 'api/recommend';
+  import {ERR_OK} from 'api/config';
+  export default{
+    created() {
+      this._getRecommend();
+    },
+    methods: {
+      _getRecommend: function () {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.slider);
+          }
+        });
+      }
+    }
+  };
 </script>
